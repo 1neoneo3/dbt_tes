047c1d72d334e74817125c68b6adaff26a31b91d
dodonpaxp@gmail.com <dodonpaxp@gmail.com>
2023-02-11 06:17:12 +0000

    costomer_ordersの定義

--- models/customers.sql
+++ models/customers.sql
@@ -5,13 +5,8 @@ orders as (
     select * from {{ ref('stg_orders') }}
 ),
 customer_orders as (
-    select
-        customer_id,
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-    from orders
-    group by 1
+    -- 顧客単位のオーダー数を求める
+    select * from {{ ref('stg_customer_orders') }}
 ),
 final as (
     select
@@ -20,7 +15,7 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        customer_orders.number_of_orders as number_of_orders
     from customers
     left join customer_orders using (customer_id)
 )
---
+++ models/stg_customer_orders.sql
@@ -0,0 +1,7 @@
+select
+    customer_id,
+    min(order_date) as first_order_date, -- 最初のオーダー日
+    max(order_date) as most_recent_order_date, -- 直近のオーダー日
+    count(order_id) as number_of_orders
+from {{ ref('stg_orders') }}
+group by 1
